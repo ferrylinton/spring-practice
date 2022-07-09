d00cce8645b53da99840ba19c984a170cbe05254
Ferry L.H. <ferrylinton27@gmail.com>
2022-07-09 14:00:26 +0700

    add mongodb crud

---
+++ spring-mongodb-reactive/src/main/java/com/kodesederhana/simplemongodb/SimpleMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.kodesederhana.simplemongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleMongodbApplication.class, args);
+	}
+
+}
